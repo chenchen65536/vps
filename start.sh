551e63a5dadeb97e9acc1c26f399628d131dda73
chenchen65536 <106053824+chenchen65536@users.noreply.github.com>
2023-11-21 16:49:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,4 @@ service nginx start
 echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
-ngrok tcp --region eu 22
-public_ip=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
-echo "Public IP Address: $public_ip"
+ngrok tcp --region eu 22 --log=stdout
